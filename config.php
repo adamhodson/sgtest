7ce8afb5f8e7f3c5b91fb3e5bc3cb804c9378153
Adam Hodson <adam@bricksandmortarweb.com>
2017-12-29 14:35:49 -0500

    another test

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define("TOKEN", "v4448407117");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
 define("REMOTE_REPOSITORY", "git@github.com:adamhodson/sgtest.git"); // The SSH URL to your repository
-define("DIR", "/home/bmdeploy/www/sgtest/");                          // The path to your repostiroy; this must begin with a forward slash (/)
+define("DIR", "/home/bmdeploy/www/sgtest");                          // The path to your repostiroy; this must begin with a forward slash (/)
 define("BRANCH", "refs/heads/master");                                 // The branch route
 define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
