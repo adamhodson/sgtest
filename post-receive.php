26fa44b7184b21292e1cc9aa21ca66a4e4cbaa60
Adam Hodson <adam@bricksandmortarweb.com>
2017-12-29 15:21:27 -0500

    update

--- post-receive.php
+++ post-receive.php
@@ -84,79 +84,51 @@
 
 
 
-/*
- * Endpoint for Github Webhook URLs
- *
- * see: https://help.github.com/articles/post-receive-hooks
- *
- */
-// script errors will be send to this email:
-$error_mail = "admin@example.com";
-function run() {
-    global $rawInput;
-    // read config.json
-    $config_filename = 'config.json';
-    if (!file_exists($config_filename)) {
-        throw new Exception("Can't find ".$config_filename);
-    }
-    $config = json_decode(file_get_contents($config_filename), true);
-    $postBody = $_POST['payload'];
-    $payload = json_decode($postBody);
-    if (isset($config['email'])) {
-        $headers = 'From: '.$config['email']['from']."\r\n";
-        $headers .= 'CC: ' . $payload->pusher->email . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-    }
-    // check if the request comes from github server
-    $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
-    if (in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
-        foreach ($config['endpoints'] as $endpoint) {
-            // check if the push came from the right repository and branch
-            if ($payload->repository->url == 'https://github.com/' . $endpoint['repo']
-                && $payload->ref == 'refs/heads/' . $endpoint['branch']) {
-                // execute update script, and record its output
-                ob_start();
-                passthru($endpoint['run']);
-                $output = ob_end_contents();
-                // prepare and send the notification email
-                if (isset($config['email'])) {
-                    // send mail to someone, and the github user who pushed the commit
-                    $body = '<p>The Github user <a href="https://github.com/'
-                    . $payload->pusher->name .'">@' . $payload->pusher->name . '</a>'
-                    . ' has pushed to ' . $payload->repository->url
-                    . ' and consequently, ' . $endpoint['action']
-                    . '.</p>';
-                    $body .= '<p>Here\'s a brief list of what has been changed:</p>';
-                    $body .= '<ul>';
-                    foreach ($payload->commits as $commit) {
-                        $body .= '<li>'.$commit->message.'<br />';
-                        $body .= '<small style="color:#999">added: <b>'.count($commit->added)
-                            .'</b> &nbsp; modified: <b>'.count($commit->modified)
-                            .'</b> &nbsp; removed: <b>'.count($commit->removed)
-                            .'</b> &nbsp; <a href="' . $commit->url
-                            . '">read more</a></small></li>';
-                    }
-                    $body .= '</ul>';
-                    $body .= '<p>What follows is the output of the script:</p><pre>';
-                    $body .= $output. '</pre>';
-                    $body .= '<p>Cheers, <br/>Github Webhook Endpoint</p>';
-                    mail($config['email']['to'], $endpoint['action'], $body, $headers);
-                }
-                return true;
-            }
-        }
-    } else {
-        throw new Exception("This does not appear to be a valid requests from Github.\n");
-    }
-}
-try {
-    if (!isset($_POST['payload'])) {
-        echo "Works fine.";
-    } else {
-        run();
-    }
-} catch ( Exception $e ) {
-    $msg = $e->getMessage();
-    mail($error_mail, $msg, ''.$e);
-}
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
+	 * How To Use:
+	 * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
+	 */
+	// The commands
+	$commands = array(
+		'echo $PWD',
+		'whoami',
+		'git reset --hard HEAD',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+	// Run the commands for output
+	$output = '';
+	foreach($commands AS $command){
+		// Run it
+		$tmp = shell_exec($command);
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+		$output .= htmlentities(trim($tmp)) . "\n";
+	}
+	// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ ____________________________
+|                            |
+| Git Deployment Script v0.1 |
+|      github.com/riodw 2017 |
+|____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
